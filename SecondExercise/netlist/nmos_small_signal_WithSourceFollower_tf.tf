6b6aa511b7437c5941d649a869cc996fe387ae39
Pietro Prandini <prandinip@gmail.com>
2019-06-04 22:39:15 +0200

    Sending the work
    Former-commit-id: d2c6e7babb74df4cf34e7136e2bc3f1c3fd90ffd

--- SecondExercise/netlist/nmos_small_signal_WithSourceFollower_tf.tf
+++ SecondExercise/netlist/nmos_small_signal_WithSourceFollower_tf.tf
@@ -1,5 +1,5 @@
---- Transfer Function ---
+       --- Transfer Function ---
 
-Transfer_function:	 0	 transfer
+Transfer_function:	 -10.962	 transfer
 vsig#Input_impedance:	 931834	 impedance
 output_impedance_at_V(vo):	 440.945	 impedance
